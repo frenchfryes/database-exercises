c5ae3b7b659511338665e4bedb429c8e4e84e50f
frenchfryes <derekwfrye@gmail.com>
2017-06-01 14:03:57 -0500

    finished before walkthrough was over

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,9 +1,35 @@
 /*Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)*/
-SELECT emp_no, first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 /*Employees whose last name starts with 'E' — 7,330 rows.*/
-SELECT emp_no, first_name, last_name FROM employees WHERE last_name LIKE 'e%';
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE 'e%';
 /*Employees hired in the 90s — 135,214 rows.*/
-SELECT emp_no, first_name, last_name FROM employees WHERE hire_date = '199%-%%-%%';
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE hire_date LIKE '199%';
 #Employees born on Christmas — 842 rows.
-
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE birth_date LIKE '%12-25';
 #Employees with a 'q' in their last name — 1,873 rows.
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE '%q%';
