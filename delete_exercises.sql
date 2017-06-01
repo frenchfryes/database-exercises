c475ce977d786e55bf39d478980e8c1be2477fd0
frenchfryes <derekwfrye@gmail.com>
2017-06-01 10:00:55 -0500

    finished select statements

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -3,7 +3,7 @@ Albums with the genre 'disco'
 Albums by 'Whitney Houston' (...or maybe an artist of your choice)*/
 USE codeup_test_db;
 
-SELECT name WHERE release_date >= '1991';
-SELECT name WHERE genre = 'disco';
-SELECT name WHERE artist = 'Spice Girls';
+SELECT name FROM albums WHERE release_date >= '1991';
+SELECT name FROM albums WHERE genre = 'disco';
+SELECT name FROM albums WHERE artist = 'Spice Girls';
 
