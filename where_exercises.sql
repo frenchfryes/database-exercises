7df9f33513bf47037de944ce663b8d9f7d8e612b
frenchfryes <derekwfrye@gmail.com>
2017-06-01 15:31:05 -0500

    Updated for gender M

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,35 +1,29 @@
 /*Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)*/
 SELECT
-  emp_no,
-  first_name,
-  last_name
+ *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE gender = 'M'
+AND (first_name = 'Irena'
+OR first_name = 'Vidya'
+Or first_name = 'Maya');
+
 /*Employees whose last name starts with 'E' — 7,330 rows.*/
 SELECT
-  emp_no,
-  first_name,
-  last_name
+*
 FROM employees
 WHERE last_name LIKE 'e%';
 /*Employees hired in the 90s — 135,214 rows.*/
 SELECT
-  emp_no,
-  first_name,
-  last_name
+  *
 FROM employees
 WHERE hire_date LIKE '199%';
 #Employees born on Christmas — 842 rows.
 SELECT
-  emp_no,
-  first_name,
-  last_name
+  *
 FROM employees
 WHERE birth_date LIKE '%12-25';
 #Employees with a 'q' in their last name — 1,873 rows.
 SELECT
-  emp_no,
-  first_name,
-  last_name
+  *
 FROM employees
 WHERE last_name LIKE '%q%';
