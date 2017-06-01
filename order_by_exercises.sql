c434979f11fc2c40a2c0c1889972e90264734f91
frenchfryes <derekwfrye@gmail.com>
2017-06-01 16:15:53 -0500

    updated for 1st name lst name order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,31 +1,27 @@
 /*Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)*/
-SELECT
- *
+SELECT *
 FROM employees
 WHERE gender = 'M'
-AND (first_name = 'Irena'
-OR first_name = 'Vidya'
-Or first_name = 'Maya');
+      AND (first_name = 'Irena'
+           OR first_name = 'Vidya'
+           OR first_name = 'Maya')
+ORDER BY first_name ASC, last_name ASC;
 
 /*Employees whose last name starts with 'E' — 7,330 rows.*/
-SELECT
-*
+SELECT *
 FROM employees
 WHERE last_name LIKE 'e%e';
 /*Employees hired in the 90s — 135,214 rows.*/
-SELECT
-  *
+SELECT *
 FROM employees
 WHERE hire_date LIKE '199%'
-AND birth_date LIKE '%12-25';
+      AND birth_date LIKE '%12-25';
 #Employees born on Christmas — 842 rows.
-SELECT
-  *
+SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25';
 #Employees with a 'q' in their last name — 1,873 rows.
-SELECT
-  *
+SELECT *
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+      AND last_name NOT LIKE '%qu%';
