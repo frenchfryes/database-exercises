268390b8f4227daec96f85427bdf83f5dcc5b682
frenchfryes <derekwfrye@gmail.com>
2017-06-01 09:41:23 -0500

    completed update exercises

--- select_exercise.sql
+++ select_exercise.sql
@@ -1,14 +1,14 @@
 USE codeup_test_db;
 
-/*The name of all albums by Pink Floyd.*/
+select 'The name of all albums by Pink Floyd.' as '---------';
 SELECT name FROM albums WHERE artist = 'pink floyd';
 /*The year Sgt. Pepper's Lonely Hearts Club Band was released*/
-SELECT release_date FROM albums WHERE artist = '';
-/*The genre for Nevermind*/
+/*SELECT release_date FROM albums WHERE artist = '';*/
+SELECT 'The genre for Nevermind' AS '----------';
 SELECT genre FROM albums WHERE name = 'Nevermind';
-/*Which albums were released in the 1990s*/
-SELECT name FROM albums WHERE release_date = '199%';
-/*Which albums had less than 20 million certified sales*/
+SELECT 'albums were released in the 1990s' AS '----------';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT 'Which albums had less than 20 million certified sales' AS '----------';
 SELECT name FROM albums WHERE sales < '20';
-/*All the albums in the rock genre. Is this all the rock albums in the table?*/
-SELECT name FROM albums WHERE genere = 'rock';
+SELECT 'the albums in the rock genre. Is this all the rock albums in the table?' AS '---------';
+SELECT name FROM albums WHERE genre = 'rock';
