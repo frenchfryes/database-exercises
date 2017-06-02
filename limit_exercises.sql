291a44e313cd72a61031b6b390e1733bddb94da5
frenchfryes <derekwfrye@gmail.com>
2017-06-02 09:34:19 -0500

    Finished #3 and Im strong that I am right and the curriculum is wrong.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,3 +5,12 @@ FROM employees
   ORDER BY last_name DESC
 LIMIT 10;
 
+#query for employees born on Christmas and hired in the 90s 1st 5
+SELECT
+  *
+FROM employees
+WHERE hire_date LIKE '199%'
+      AND birth_date LIKE '%12-25'
+  ORDER BY hire_date ASC
+LIMIT 5;
+
