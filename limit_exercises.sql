6a3bb3dbb4a92ec1de5603e43ad00e0047c6c8bf
frenchfryes <derekwfrye@gmail.com>
2017-06-02 10:25:54 -0500

    Finished Exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,12 +5,11 @@ FROM employees
   ORDER BY last_name DESC
 LIMIT 10;
 
-#query for employees born on Christmas and hired in the 90s 1st 5
-SELECT
-  *
+#query for employees born on Christmas and hired in the 90s
+SELECT *
 FROM employees
 WHERE hire_date LIKE '199%'
       AND birth_date LIKE '%12-25'
-  ORDER BY hire_date ASC;
-/*LIMIT 5 OFFSET 50*/
+ORDER BY hire_date DESC, birth_date DESC
+LIMIT 5 OFFSET 45;
 
